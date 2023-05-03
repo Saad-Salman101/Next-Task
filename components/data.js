10b8a508d768d3d86fafe750e8c36c7a657770f4
Saad <saad.salman98@gmail.com>
2023-05-03 20:07:45 +0500

    initial commit

--- components/data.js
+++ components/data.js
@@ -1,20 +1,18 @@
 const imageslide = [
     {
-      url: "/../public/images/photo-1564.jpg",
+      url: "/images/photo-1564.jpg",
       title: "React Background full Screen Automatic Image Slider 1",
       body:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et",
     },
     {
-      url:
-        "/../public/images/photo2.png",
+      url:"/images/photo2.png",
       title: "React Background full Screen Automatic Image Slider 2",
       body:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et",
     },
     {
-      url:
-        "/../public/images/photo3.jpg",
+      url: "/images/photo3.jpg",
       title: "React Background full Screen Automatic Image Slider 3",
       body:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et",
