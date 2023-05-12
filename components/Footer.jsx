8e7a0c8fc467b35b28305f46cdbef82bf0c5d0b7
Saad <saad.salman98@gmail.com>
2023-05-12 18:03:33 +0500

    5th commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,64 +1,71 @@
 import React from "react";
 import Timer from "./SmallTimmer";
 
-const Footer = ({myprops}) => {
-    // let myprops = {
-    //     id: 12331,
-    //     ticket_id: 5,
-    //     ticketprice: 30.0,
-    //     expiry: "2023-06-27 00:00:00",
-    // };
-    const targetDate = new Date(myprops.expiry);
-    return (
-        <>
-            <div className="mt-12">
-                <div className="w-full flex justify-center items-center">
-                <div className="w-[70%] border-t-2 border-custom-golden text-white">
-                    .
+const Footer = ({ myprops }) => {
+  // let myprops = {
+  //     id: 12331,
+  //     ticket_id: 5,
+  //     ticketprice: 30.0,
+  //     expiry: "2023-06-27 00:00:00",
+  // };
+  const targetDate = new Date(myprops.expiry);
+  return (
+    <>
+      <div className="mt-12">
+        <div className="w-full flex justify-center items-center">
+          <div className="w-[70%] border-t-2 border-custom-golden text-white">
+            .
+          </div>
+        </div>
+        <div className="flex justify-between mt-5 ">
+          <div className="ml-10 text-lg">
+            <div className="text-gray-400  tracking-[3px]">#ZM{myprops.id}</div>
+            <div className="text-gray-400 tracking-[3px]">{myprops.id}</div>
+          </div>
+          <div className="flex-col justify-end">
+            <div className="flex  uppercase mr-4">
+              <div>
+                <div className="flex flex-col bg-black p-2 pl-8 pr-8 justify-around rounded-md">
+                  <div className="text-white text-lg">
+                    {myprops.ticket_id} ticket(s)
+                  </div>
+                  <div className="text-white m-4 text-lg font-bold">
+                    €{" "}
+                    {myprops.ticketprice == null ? "Null" : myprops.ticketprice}
+                  </div>
+                  <div className="text-white text-sm text-center">
+                    #ZM{myprops.id}
+                  </div>
                 </div>
+                <div className="bg-black text-center text-white mt-2 p-1 rounded-md uppercase">
+                  <button> BUY NOW</button>
                 </div>
-            <div className="flex justify-between mt-5 ">
-                <div className="ml-10 text-lg">
-                    <div className="text-gray-400  tracking-[3px]">#ZM{myprops.id}</div>
-                    <div className="text-gray-400 tracking-[3px]">{myprops.id}</div>
-                </div>
-                <div className="flex-col justify-end">
-                    <div className="flex  uppercase ml-4">
-                        <div>
-                            <div className="flex flex-col bg-black p-2 pl-8 pr-8 justify-around rounded-md">
-                                <div className="text-white text-lg">
-                                {myprops.ticket_id} ticket(s)
-                                </div>
-                                <div className="text-white m-4 text-lg font-bold">
-  € {myprops.ticketprice == null ? "Null" : myprops.ticketprice}
-</div>
-                                <div className="text-white text-sm text-center">#ZM{myprops.id}</div>
-                            </div>
-                            <div className="bg-black text-center text-white mt-2 p-1 rounded-md uppercase">
-                                <button> BUY NOW</button>
-                            </div>
-                        </div>
+              </div>
 
-                        <div className="flex flex-col ml-3 bg-black justify-between text-xl mr-0 p-2 rounded-md">
-                            <div className="text-white "> <button>+</button></div>
-                            <div className="text-white"> <button>-</button></div>
-                        </div>
-                    </div>
-                    <div>
-                        <Timer targetDate={targetDate} />
-                    </div>
-                    <div>
-                        <button className="uppercase text-white bg-black mt-3 p-3 pt-6 pb-6 pl-8 pr-8 ml-12 rounded-md">
-                            enter <br /> now
-                        </button>
-                    </div>
-
+              <div className="flex flex-col ml-3 bg-black justify-between text-xl mr-0 p-2 rounded-md">
+                <div className="text-white ">
+                  {" "}
+                  <button>+</button>
                 </div>
-
+                <div className="text-white">
+                  {" "}
+                  <button>-</button>
+                </div>
+              </div>
             </div>
+            <div>
+              <Timer targetDate={targetDate} />
             </div>
-        </>
-    );
+            <div>
+              <button className="uppercase text-white bg-black mt-3 p-3 pt-6 pb-6 pl-8 pr-8 ml-12 rounded-md">
+                enter <br /> now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
--- components/ImageGallery2.jsx
+++ components/ImageGallery2.jsx
@@ -4,34 +4,34 @@ import Image from 'next/image';
 const ImageGallery2 = ({ props }) => {
 
   return (
-<div className="container mx-auto">
-  {/* heading text */}
-  <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4">
+    <div className="container mx-auto">
+      {/* heading text */}
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
   {props.map((imageSrc, index) => (
-  <li
-    className={`relative bg-black ${
-      index === 0 || index === 1 ? 'col-span-2' :
-      index === 2 || index === 3 || index === 4 ? 'col-span-1' :
-      index === 5 || index === 6 ? 'col-span-2' :
-      index === 7 ? 'col-span-4' : 'col-span-1'
-    }`}
-    key={imageSrc}
-  >
-    <div style={{ position: 'relative', width: '100%', height: '400px' }}>
-      <Image
-        src={imageSrc}
-        alt=""
-        layout="fill"
-        objectFit="cover"
-      />
+    <li
+      className={`relative bg-black ${
+        index % 8 === 0 || index % 8 === 1 ? 'col-span-2' :
+        index % 8 === 2 || index % 8 === 4 ? 'col-span-1' :
+        index % 8 === 3 || index % 8 === 5 ? 'col-span-2' :
+        index % 8 === 6 ? 'col-span-1' : 'col-span-2'
+      }`}
+      key={imageSrc}
+    >
+      <div style={{ position: 'relative', width: '100%', height: '400px' }}>
+        <Image
+          src={imageSrc}
+          alt=""
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <div className="absolute inset-0 bg-black bg-opacity-40 hover:bg-opacity-60 transition duration-300"></div>
+    </li>
+  ))}
+</ul>
+
+
     </div>
-    <div className="absolute inset-0 bg-black bg-opacity-40 hover:bg-opacity-60 transition duration-300"></div>
-  </li>
-))}
-
-  </ul>
-</div>
-
   );
 };
 
